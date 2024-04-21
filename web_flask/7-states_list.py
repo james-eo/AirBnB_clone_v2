5d849d97d1411ff7c3b88c5b3fce9221e8206caa
james-eo <jameseokonkwo@gmail.com>
2024-04-21 14:12:59 +0100

    List of states

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,28 +1,27 @@
 #!/usr/bin/python3
-# Starts a Flask web application
+""" Script to start a Flask web application """
 
 from flask import Flask, render_template
 from models import storage
 from models.state import State
 
 app = Flask(__name__)
-app.config["STRICT_SLASHES"] = False
+
+
+@app.route('/states_list', strict_slashes=False)
+def states_list():
+    """ Route to display a HTML page with all states """
+    states = storage.all(State).values()
+    states = sorted(states, key=lambda state: state.name)
+
+    return render_template('7-states_list.html', states=states)
 
 
 @app.teardown_appcontext
 def teardown_db(exception):
-    """Close SQLAchemy session after each request"""
+    """ Teardown method to close the storage """
     storage.close()
 
 
-@app.route("/states_list", strict_slashes=False)
-def states_list():
-    """display a HTML page with a list of states"""
-    states = storage.all(State).values()
-    # Sort by name (A->Z)
-    sorted_states = sorted(states, key=lambda x: x.name)
-    return render_template("7-states_list.html", states=sorted_states)
-
-
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host='0.0.0.0', port=5000)
