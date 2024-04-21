ae3134d9cb66694455b55d66b970acd589b0a197
james-eo <jameseokonkwo@gmail.com>
2024-04-21 21:11:57 +0100

    List of states

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -5,6 +5,7 @@ from flask import Flask, render_template
 from models import storage
 from models.state import State
 
+'''
 app = Flask(__name__)
 
 
@@ -24,4 +25,26 @@ def teardown_db(exception):
 
 
 if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
+'''
+
+app = Flask(__name__)
+app.config['STRICT_SLASHES'] = False
+
+
+@app.teardown_appcontext
+def teardown_db(exception):
+    """ Teardown method to close the storage """
+    storage.close()
+
+
+@app.route('/states_list', strict_slashes=False)
+def states_list():
+    """ Route to display a HTML page with all states """
+    states = storage.all(State)
+    sorted_states = sorted(states, key=lambda x: x.name)
+    return render_template('7-states_list.html', states=sorted_states)
+
+
+if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
